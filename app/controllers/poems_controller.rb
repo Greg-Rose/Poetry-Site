2f6df5363da48f1d9fddb4ff3f2551eb4ff13862
Greg Rose <gregrose628@gmail.com>
2017-05-30 03:44:26 -0400

    added pagination with will_paginate gem

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,6 @@
 class PoemsController < ApplicationController
   def index
-    @poems = Poem.order(:sort)
+    @poems = Poem.paginate(page: params[:page], per_page: 8).order(:sort)
   end
 
   def show
